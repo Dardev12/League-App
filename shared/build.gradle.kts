27ed093c40b50b53a80d2ce6f10c89c14e186fd5
Darren Lambert <73501755+Dardev12@users.noreply.github.com>
2025-10-10 19:52:42 -0400

    Cocaopods Setup iOS ✅

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,11 +1,42 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    kotlin("native.cocoapods") version "2.2.20"
 }
 
 kotlin {
+    cocoapods {
+        // Required properties
+        // Specify the required Pod version here
+        // Otherwise, the Gradle project version is used
+        version = "1.0"
+        summary = "Shared"
+        homepage = "Link to a Kotlin/Native module homepage"
+        podfile = project.file("../iosApp/Podfile")
+        ios.deploymentTarget = "18.2"
+
+        framework {
+            // Required properties
+            // Framework name configuration. Use this property instead of deprecated 'frameworkName'
+            baseName = "shared"
+
+            // Optional properties
+            // Specify the framework linking type. It's dynamic by default.
+            isStatic = true
+            // Dependency export
+            // Uncomment and specify another project module if you have one:
+            // export(project(":<your other KMP module>"))
+            //transitiveExport = false // This is default.
+        }
+
+        // Maps custom Xcode configuration to NativeBuildType
+        xcodeConfigurationToNativeBuildType["CUSTOM_DEBUG"] = NativeBuildType.DEBUG
+        xcodeConfigurationToNativeBuildType["CUSTOM_RELEASE"] = NativeBuildType.RELEASE
+    }
+
     androidTarget {
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
